6bde50886cb3db9e2da64f931a8c3f99823bc66b
Wes Young <wes@barely3am.com>
2016-10-22 14:02:39 -0400

    fixing setup test

--- setup.py
+++ setup.py
@@ -20,8 +20,11 @@ if sys.argv[-1] == 'test':
         err_msg = e.message.replace("No module named ", "")
         msg = "%s is not installed. Install your test requirements." % err_msg
         raise ImportError(msg)
-    os.system('py.test test -v --cov=csirtg_smrt --cov-fail-under=45')
-    sys.exit()
+    r = os.system('py.test test -v --cov=csirtg_smrt --cov-fail-under=45')
+    if r == 0:
+        sys.exit()
+    else:
+        raise RuntimeError('tests failed')
 
 package_data = {}
 if sys.platform == 'nt':
