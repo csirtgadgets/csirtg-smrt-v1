773a587cd323ce51a72abeeed9c9455e406f3361
Wes Young <wes@barely3am.com>
2017-02-28 14:05:58 -0500

    fixing fireball default

--- csirtg_smrt/constants.py
+++ csirtg_smrt/constants.py
@@ -34,7 +34,7 @@ if sys.version_info > (3,):
     PYVERSION = 3
 
 
-FIREBALL_SIZE = os.environ.get('CSIRTG_SMRT_FIREBALL_SIZE', 500)
+FIREBALL_SIZE = os.environ.get('CSIRTG_SMRT_FIREBALL_SIZE', 100)
 
 ROUTER_ADDR = "ipc://{}".format(os.path.join(RUNTIME_PATH, 'router.ipc'))
 ROUTER_ADDR = os.environ.get('CIF_ROUTER_ADDR', ROUTER_ADDR)
