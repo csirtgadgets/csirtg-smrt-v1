b9e2c99f0032e05f912c828671b6338c26adb49b
Wes Young <wes@barely3am.com>
2017-04-29 10:00:43 -0700

    fixing tokens

--- csirtg_smrt/client/zcif.py
+++ csirtg_smrt/client/zcif.py
@@ -3,11 +3,12 @@ from cifsdk.client.http import HTTP as HTTPClient
 import os
 
 REMOTE = os.environ.get('CSIRTG_SMRT_CIF_REMOTE', 'http://localhost:5000')
+TOKEN = os.getenv('CIF_TOKEN')
 
 
 class CIF(HTTPClient):
 
-    def __init__(self, remote=None, token=None, **kwargs):
+    def __init__(self, remote=None, token=TOKEN, **kwargs):
         if not remote:
             remote = REMOTE
 
