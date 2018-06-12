b9c6d1284593371b66ee776a1bfddb5e98ad2dff
Wes Young <wes@barely3am.com>
2018-06-12 13:50:38 -0400

    tweaking test

--- test/smrt/test_csv_quoted.py
+++ test/smrt/test_csv_quoted.py
@@ -12,3 +12,6 @@ def test_smrt_csv_quoted():
         for r, f in s.load_feeds('test/smrt/rules/csv_quoted.yml', feed='test'):
             x = list(s.process(r, f))
             assert len(x) > 0
+
+            assert x[0].description == '1.2.3, aaabbbcccddd'
+            assert x[1].description == '1,2,3'
