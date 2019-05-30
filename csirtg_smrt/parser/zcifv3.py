f89c31a6de8e3fc7f97b575c201851cada21018f
Scott Finlon <finlon@gmail.com>
2019-05-30 18:21:07 -0400

    fix: cifv3 parser broken for ES backend instances (#275)
    * fix cifv3 parser
    
    * fix cifv3 parser

--- csirtg_smrt/parser/zcifv3.py
+++ csirtg_smrt/parser/zcifv3.py
@@ -1,9 +1,13 @@
 import copy
 import json
 from csirtg_smrt.parser import Parser
+from cifsdk.constants import PYVERSION
 import logging
 import os
 from pprint import pprint
+
+if PYVERSION == 3:
+    basestring = (str, bytes)
 
 TRACE = os.environ.get('CSIRTG_SMRT_PARSER_TRACE')
 
@@ -21,14 +25,8 @@ class cifv3(Parser):
 
     def process(self):
         defaults = self._defaults()
-        map = self.rule.feeds[self.feed].get('map')
         values = self.rule.feeds[self.feed].get('values')
         envelope = self.rule.feeds[self.feed].get('envelope')
-
-        try:
-            basestring
-        except NameError:
-            basestring = str
 
         for l in self.fetcher.process():
             try:
@@ -36,8 +34,8 @@ class cifv3(Parser):
             except ValueError as e:
                 logger.error('json parsing error: {}'.format(e))
                 continue
-            if l.get('data') and isinstance(l['data'], basestring) and l['data'].startswith(
-                    '{"hits":{"hits":[{"_source":'):
+
+            if l.get('data') and isinstance(l['data'], basestring) and l['data'].startswith('{"hits":{"hits":[{"_source":'):
                 l['data'] = json.loads(l['data'])
                 l['data'] = [r['_source'] for r in l['data']['hits']['hits']]
 
@@ -46,13 +44,19 @@ class cifv3(Parser):
 
             if l.get('data') != '{}':
                 for e in l['data']:
+                    if isinstance(e['group'], list):
+                        e['group'] = e['group'][0]
+
                     i = copy.deepcopy(defaults)
-                    if map:
-                        for x, c in enumerate(map):
-                            i[values[x]] = e[c]
+
+                    if values:
+                        for value in values:
+                            i[value] = e[value]
+                    else:
+                        for value in e.keys():
+                            i[value] = e[value]
                     yield i
             else:
                 return
 
 Plugin = cifv3
-
